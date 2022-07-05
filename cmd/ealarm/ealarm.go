e118de2d1d79070d266352c21e1ccc2c183a60a7
Alberto Elorza <albertoelorza2002@gmail.com>
2022-07-05 12:51:07 +0200

    Basic controller implementation

--- cmd/ealarm/ealarm.go
+++ cmd/ealarm/ealarm.go
@@ -1,4 +1,35 @@
 package main
 
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/albertoaer/ealarm/internal/controller"
+)
+
+func getInput() (time.Duration, error) {
+	hours := flag.Int("h", 0, "Number of hours")
+	minutes := flag.Int("m", 0, "Number of minutes")
+	seconds := flag.Int("s", 0, "Number of seconds")
+	flag.Parse()
+	dur := time.Duration(*hours)*time.Hour + time.Duration(*minutes)*time.Minute + time.Duration(*seconds)*time.Second
+	if dur == 0 {
+		return dur, errors.New("Trying to elapse time 0")
+	}
+	return dur, nil
+}
+
 func main() {
+	d, err := getInput()
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err.Error())
+		flag.PrintDefaults()
+		return
+	}
+	cnt := controller.New(d)
+	if err = cnt.Start(); err != nil {
+		fmt.Printf("ERROR: %s\n", err.Error())
+	}
 }
---
+++ internal/controller/controller.go
@@ -0,0 +1,32 @@
+package controller
+
+import (
+	"errors"
+	"time"
+)
+
+type Controller struct {
+	lapse  time.Duration
+	action func(chan bool)
+}
+
+func New(lapse time.Duration) *Controller {
+	return &Controller{lapse, nil}
+}
+
+func (controller *Controller) SetAction(action func(chan bool)) {
+	controller.action = action
+}
+
+func (controller *Controller) Start() error {
+	n := make(chan bool, 1)
+	if controller.action == nil {
+		return errors.New("No action associated")
+	}
+	n <- true
+	for <-n {
+		time.Sleep(controller.lapse)
+		controller.action(n)
+	}
+	return nil
+}
