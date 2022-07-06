842e4408cbbdea8c89572f5729475516e82394c9
Alberto Elorza <albertoelorza2002@gmail.com>
2022-07-06 19:24:38 +0200

    load configuration

--- cmd/ealarm/ealarm.go
+++ cmd/ealarm/ealarm.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io/fs"
+	"os"
 	"time"
 
 	"github.com/albertoaer/ealarm/controller"
@@ -11,23 +13,35 @@ import (
 	"github.com/albertoaer/ealarm/ui"
 )
 
-func getInput(config *AlarmConfiguration) (err error) {
+func loadConfiguration(config *AlarmConfiguration) (err error) {
 	hours := flag.Int("h", 0, "Number of hours")
 	minutes := flag.Int("m", 0, "Number of minutes")
 	seconds := flag.Int("s", 0, "Number of seconds")
+	msg := flag.String("d", "ALARM!", "Display message")
+	track := flag.String("t", "", "Track file as alarm tone")
 	flag.Parse()
 	dur := time.Duration(*hours)*time.Hour + time.Duration(*minutes)*time.Minute + time.Duration(*seconds)*time.Second
-	if dur == 0 {
-		err = errors.New("Trying to elapse time 0")
-	} else {
-		config.Duration = dur
+	if dur <= 0 {
+		err = errors.New("Elapsed time must be positive and greater than 0 seconds")
+		return
 	}
+	config.Duration = dur
+	if len(*msg) == 0 {
+		err = errors.New("Message must not be empty")
+		return
+	}
+	config.Message = *msg
+	if _, err = os.Stat(*track); errors.Is(err, fs.ErrNotExist) {
+		err = errors.New("A valid track is required")
+		return
+	}
+	config.Track = *track
 	return
 }
 
 func main() {
 	config := AlarmConfiguration{}
-	err := getInput(&config)
+	err := loadConfiguration(&config)
 	if err != nil {
 		fmt.Printf("ERROR: %s\n", err.Error())
 		flag.PrintDefaults()
