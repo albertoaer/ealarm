cc8fb5df225d01acb4fd5fa918b4c50b8a5273d3
Alberto Elorza <albertoelorza2002@gmail.com>
2022-07-07 19:27:00 +0200

    alarm gui updated

--- ui/alarm_ui.go
+++ ui/alarm_ui.go
@@ -1,30 +1,57 @@
 package ui
 
 import (
+	"image/color"
+
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 	. "github.com/albertoaer/ealarm/core"
 )
 
 type AlarmUI struct {
 	w      fyne.Window
 	config *AlarmConfiguration
+	signal *chan bool
 }
 
 func (ui *UI) NewAlarm(config *AlarmConfiguration) *AlarmUI {
 	w := ui.app.NewWindow("Alarm")
 	w.SetFixedSize(true)
-	w.CenterOnScreen()
 	w.Resize(fyne.NewSize(400, 400))
-	return &AlarmUI{w, config}
+	w.SetPadded(false)
+
+	text := canvas.NewText(config.Message, color.White)
+	text.TextSize = 32
+
+	alarm := &AlarmUI{w, config, nil}
+
+	endfn := func(s bool) func() {
+		return func() {
+			w.Hide()
+			config.Track.Stop()
+			*alarm.signal <- s
+		}
+	}
+
+	w.SetContent(container.NewCenter(container.NewVBox(container.NewCenter(text),
+		widget.NewSeparator(),
+		container.NewBorder(
+			nil,
+			nil,
+			widget.NewButton("STOP", endfn(false)),
+			widget.NewButton("CONTINUE", endfn(true)),
+		))))
+
+	w.SetCloseIntercept(endfn(true))
+
+	w.CenterOnScreen()
+	return alarm
 }
 
 func (alarm *AlarmUI) Show(n chan bool) {
 	alarm.config.Track.PlayLoop()
-	alarm.w.SetCloseIntercept(func() {
-		alarm.w.Hide()
-		alarm.config.Track.Stop()
-		n <- true
-	})
-
+	alarm.signal = &n
 	alarm.w.Show()
 }
