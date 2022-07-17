6442d9c88c5dc29bf1cd490744803b1dfec7fd09
Alberto Elorza <albertoelorza2002@gmail.com>
2022-07-17 17:24:01 +0200

    presets and better flags

--- cmd/ealarm/ealarm.go
+++ cmd/ealarm/ealarm.go
@@ -4,14 +4,42 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/albertoaer/ealarm/audio"
 	. "github.com/albertoaer/ealarm/core"
+	. "github.com/albertoaer/ealarm/presets"
 	"github.com/albertoaer/ealarm/ui"
 )
 
-func loadConfiguration(config *AlarmConfiguration) (err error) {
+func getProfile(presets *Presets) error {
+	if flag.NArg() == 0 {
+		return nil
+	}
+	if flag.NArg() > 1 {
+		return errors.New("Expecting just one non-flag arg as profile name")
+	}
+	m, in := presets.Profiles[flag.Arg(0)]
+	if !in {
+		return fmt.Errorf("Unknown profile: %s", flag.Arg(0))
+	}
+	write := make(map[string]string)
+	for k, v := range m {
+		write[k] = v
+	}
+	flag.Visit(func(f *flag.Flag) {
+		delete(write, f.Name)
+	})
+	for k, v := range write {
+		if e := flag.Set(k, v); e != nil {
+			return e
+		}
+	}
+	return nil
+}
+
+func loadConfiguration(config *AlarmConfiguration, presets *Presets) (err error) {
 	hours := flag.Int("h", 0, "Number of hours")
 	minutes := flag.Int("m", 0, "Number of minutes")
 	seconds := flag.Int("s", 0, "Number of seconds")
@@ -19,15 +47,16 @@ func loadConfiguration(config *AlarmConfiguration) (err error) {
 	track := flag.String("t", "", "Track file as alarm tone")
 	times := flag.Int("n", -1, "Number of times to play, if it's negative it will loop infinitely")
 	flag.Parse()
+	if err = getProfile(presets); err != nil {
+		return
+	}
 	dur := time.Duration(*hours)*time.Hour + time.Duration(*minutes)*time.Minute + time.Duration(*seconds)*time.Second
 	if dur < 0 {
-		err = errors.New("Elapsed time must not be negative")
-		return
+		return errors.New("Elapsed time must not be negative")
 	}
 	config.Duration = dur
 	if len(*msg) == 0 {
-		err = errors.New("Message must not be empty")
-		return
+		return errors.New("Message must not be empty")
 	}
 	config.Message = *msg
 	if config.Track, err = audio.From(*track); err != nil {
@@ -39,9 +68,15 @@ func loadConfiguration(config *AlarmConfiguration) (err error) {
 
 func main() {
 	config := AlarmConfiguration{}
-	err := loadConfiguration(&config)
+	presets := LoadPresets()
+	err := loadConfiguration(&config, presets)
 	if err != nil {
 		fmt.Printf("ERROR: %s\n", err.Error())
+		var optional_flags string
+		flag.VisitAll(func(f *flag.Flag) {
+			optional_flags += "[-" + f.Name + "] "
+		})
+		fmt.Printf("Usage: %s %s[profile]\n", os.Args[0], optional_flags)
 		flag.PrintDefaults()
 		return
 	}
---
+++ presets/presets.go
@@ -0,0 +1,38 @@
+package presets
+
+import (
+	"encoding/json"
+	"errors"
+	"io/fs"
+	"io/ioutil"
+	"os"
+)
+
+type Presets struct {
+	Actions  map[string]string            `json:"actions,omitempty"`
+	Profiles map[string]map[string]string `json:"profiles,omitempty"`
+}
+
+const presets_file string = "./presets.json"
+
+var presets *Presets
+
+func LoadPresets() *Presets {
+	if presets == nil {
+		loaded := Presets{}
+		if _, e := os.Stat(presets_file); errors.Is(e, fs.ErrNotExist) {
+			if e := ioutil.WriteFile(presets_file, []byte("{}"), os.ModePerm); e != nil {
+				panic(e)
+			}
+		}
+		data, e := ioutil.ReadFile(presets_file)
+		if e != nil {
+			panic(e)
+		}
+		if e := json.Unmarshal(data, &loaded); e != nil {
+			panic(e)
+		}
+		presets = &loaded
+	}
+	return presets
+}
